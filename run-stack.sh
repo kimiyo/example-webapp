f6cda62e85f83c6d87d41f77d7da23535d6a6d83
Jong Hoon Kim <kimiyo@naver.com>
2018-06-24 11:07:03 +0900

    add run-atack.sh for creating Docker Enc

--- run-stack.sh
+++ run-stack.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 aws cloudformation $ACTION \
-    --region us-east-1 \
+    --region ap-northeast-2 \
     --stack-name $STACK_NAME \
     --template-body file://service.yaml \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameters \
-    ParameterKey=DockerImage,ParameterValue=089778365617.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
+    ParameterKey=DockerImage,ParameterValue=140267441671.dkr.ecr.ap-northeast-2.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-c92271a1 \
     ParameterKey=Cluster,ParameterValue=default \
-    ParameterKey=Listener,ParameterValue=$ALB_LISTENER_ARN
+    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:ap-northeast-2:140267441671:listener/app/production-website/60cbfdb1a32559f2/e1df31cd6c9ab311
